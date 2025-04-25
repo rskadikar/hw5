2c423e3e804e0f7f4b23993074e9e162af741dde
Rachana Kadikar <kadikar@usc.edu>
2025-04-25 13:33:52 -0700

    final

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,10 +21,36 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool hitLimit(
+    const size_t maxShifts,
+    DailySchedule& sched,
+    Worker_T id);
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int day, int pos);
 
 
 // Add your implementation of schedule() and other helper functions here
-
+/**
+ * @brief Produces a work schedule given worker availability,
+ *        the number of needed workers per day, and the maximum
+ *        shifts any single worker is allowed. Returns true if
+ *        and the valid schedule if a solution exists, and false
+ *        otherwise.
+ *
+ * @param [in]  avail n x k vector of vectors (i.e. matrix) of the availability
+ *                    of the k workers for each of the n days
+ * @param [in]  dailyNeed Number of workers needed per day (aka d)
+ * @param [in]  maxShifts Maximum shifts any worker is allowed over
+ *                        the n day period (aka m)
+ * @param [out] sched n x d vector of vectors indicating the d workers
+ *                    who are scheduled to work on each of the n days
+ * @return true if a solution exists; sched contains the solution
+ * @return false if no solution exists; sched is undefined (can be anything)
+ */
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -37,9 +63,46 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    sched.resize(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID));
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0);
 }
 
+bool hitLimit(
+    const size_t maxShifts,
+    DailySchedule& sched,
+    Worker_T id)
+{
+    size_t count = 0;
+    for (size_t i = 0; i < sched.size(); i++) {
+        for (size_t j = 0; j < sched[i].size(); j++) {
+            if (sched[i][j] == id) {
+                count++;
+                if (count >= maxShifts) return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int day, int pos)
+{
+    if (day == avail.size()) {
+        return true;
+    }
+    if (pos == dailyNeed) {
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, day + 1, 0);
+    }
+    for (Worker_T id = 0; id < avail[0].size(); id++) {
+        if (avail[day][id] == 1 && !hitLimit(maxShifts, sched, id)) {
+            sched[day][pos] = id;
+            if (scheduleHelper(avail, dailyNeed, maxShifts, sched, day, pos + 1)) return true;
+            else sched[day][pos] = INVALID_ID;
+        }
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+                  const std::string& in,
+                  std::string temp,
+                  const std::string& floating,
+                  const std::set<std::string>& dict,
+                  std::set<std::string>& res,
+                  int pos);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +28,48 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> res;
+    string temp = in;
+    wordleHelper(in, temp, floating, dict, res, 0);
+    return res;
 }
 
 // Define any helper functions here
+void wordleHelper(
+    const std::string& in,
+    std::string temp,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& res,
+    int pos)
+{
+    if (pos == in.length()) { // aka, gone off the ledge
+        if (floating.empty() && dict.find(temp) != dict.end()) {
+            res.insert(temp);
+        }
+    }
+    else if (in[pos] == '-') {
+        for (size_t i = 0; i < floating.length(); i++) {
+            char letter = floating[i];
+            temp[pos] = letter;
+            string float_store = floating;
+            float_store.erase(i, 1);
+            wordleHelper(in, temp, float_store, dict, res, pos + 1);
+        }
+        int pos_left = in.length() - pos;
+        if (pos_left > floating.length()) {
+            for (int i = 0; i < 26; i++) {
+                char c = 'a' + i;
+                if (floating.find(c) == string::npos) {
+                    temp[pos] = c;
+                    wordleHelper(in, temp, floating, dict, res, pos + 1);
+                }
+            }
+        }
+    }
+    else {
+        temp[pos] = in[pos];
+        wordleHelper(in, temp, floating, dict, res, pos + 1);
+    }
+}
+
